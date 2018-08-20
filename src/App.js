59614b8dfba8c142962b0ce957cb20f775ac2490
Yves Godart <yves@Yvess-MBP.lan>
2018-08-20 10:15:25 +0200

    commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from 'axios'
+
+const electron = window.require('electron')
+const ipcRenderer = electron.ipcRenderer
+
+class App extends Component {
+  state = {
+    posts: []
+  }
+  componentDidMount() {
+    axios.get("https://reddit.com/r/aww.json")
+    .then(response => {
+      this.setState({posts: response.data.data.children})
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  }
+
+  showImage = (image) => {
+    ipcRenderer.send('toggle-image',image)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ul className="list-group list-group-flush">
+        {this.state.posts.map(post =>{
+          return <li 
+                      onClick={() => this.showImage(post.data.preview.images[0].source.url)}
+                      key={post.data.id} 
+                      className="list-group-item flex-container"
+                  >
+            <img src={post.data.thumbnail} className="thumbnail"></img>
+            <div>{post.data.title}</div>
+            </li>
+        })}
+          
+          
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Image.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+const electron = window.require('electron')
+const ipcRenderer = electron.ipcRenderer
+class Image extends Component {
+    state={
+        imageUrl: ''
+    }
+
+    componentDidMount() {
+        ipcRenderer.on('image',(event, arg)=>{
+            this.setState({imageUrl: arg})
+        })
+    }
+
+    render() { 
+        return ( 
+            <img src={this.state.imageUrl} style={{width: '100%'}}></img>
+         );
+    }
+}
+ 
+export default Image;
